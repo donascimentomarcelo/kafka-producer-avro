30d35850aaabfb4df35ef48985d3d406922b4a21
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2024-05-17 22:11:35 -0300

    first commit

---
+++ src/main/java/com/api/kafkaproduceravro/KafkaProducerAvroApplication.java
@@ -0,0 +1,13 @@
+package com.api.kafkaproduceravro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerAvroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerAvroApplication.class, args);
+	}
+
+}
